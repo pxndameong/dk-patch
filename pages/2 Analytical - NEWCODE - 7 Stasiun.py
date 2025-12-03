ebe564aae30ca6eb729f7bb78a0e6ef7ae1767c2
pxndameong <ammartsaqib6@gmail.com>
2025-12-03 08:30:29 +0700

    patch

--- pages/2 Analytical - NEWCODE - 7 Stasiun.py
+++ pages/2 Analytical - NEWCODE - 7 Stasiun.py
@@ -385,7 +385,7 @@ def plot_comparative_charts_monthly(tahun_start: int, bulan_start: int, tahun_en
         return
 
     fig_scatter, axes = plt.subplots(2, 3, figsize=(15, 10))
-    axes = axes.flatten()  # ← ini WAJIB supaya 'axes' jadi list 1D
+    axes = axes.flatten() 
     plt.style.use('ggplot')
 
     scatter_color_map = {
